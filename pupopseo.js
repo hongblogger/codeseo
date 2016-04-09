3f5081a26f3ebec6ada6c6bc7dd1684a9197ae49
hongblogger <seobaotop1@gmail.com>
2016-04-09 15:39:30 +0700

    Create pupopseo.js

---
+++ pupopseo.js
@@ -0,0 +1,123 @@
+//please contact me giaiphapthuonghieu.org@gmail.com
+//website http://www.batchuontyren.com/ or http://www.bloghong.com
+
+(function($) {
+    $.fn.bloghong = function(options) {        
+        var defaults = {  
+	    	animation: 'fadeAndPop', //fade, fadeAndPop, none
+		    animationspeed: 300, //how fast animtions are
+		    closeonbackgroundclick: true, //if you click background will modal close?
+		    dismissmodalclass: 'close-bloghong-modal' //the class of a button or element that will close an open modal
+    	}; 
+
+        var options = $.extend({}, defaults, options); 
+	
+        return this.each(function() {
+        	var modal = $(this),
+        		topMeasure  = parseInt(modal.css('top')),
+				topOffset = modal.height() + topMeasure,
+          		locked = false,
+				modalBG = $('.bloghong-modal-bg');
+
+			if(modalBG.length == 0) {
+				modalBG = $('<div class="bloghong-modal-bg" />').insertAfter(modal);
+			}		    
+			modal.bind('bloghong:open', function () {
+			  modalBG.unbind('click.modalEvent');
+				$('.' + options.dismissmodalclass).unbind('click.modalEvent');
+				if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modal.css({'top': $(document).scrollTop()-topOffset, 'opacity' : 0, 'visibility' : 'visible'});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"top": $(document).scrollTop()+topMeasure + 'px',
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					}
+					if(options.animation == "fade") {
+						modal.css({'opacity' : 0, 'visibility' : 'visible', 'top': $(document).scrollTop()+topMeasure});
+						modalBG.fadeIn(options.animationspeed/2);
+						modal.delay(options.animationspeed/2).animate({
+							"opacity" : 1
+						}, options.animationspeed,unlockModal());					
+					} 
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'visible', 'top':$(document).scrollTop()+topMeasure});
+						modalBG.css({"display":"block"});	
+						unlockModal()				
+					}
+				}
+				modal.unbind('bloghong:open');
+			}); 	
+
+			modal.bind('bloghong:close', function () {
+			  if(!locked) {
+					lockModal();
+					if(options.animation == "fadeAndPop") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"top":  $(document).scrollTop()-topOffset + 'px',
+							"opacity" : 0
+						}, options.animationspeed/2, function() {
+							modal.css({'top':topMeasure, 'opacity' : 1, 'visibility' : 'hidden'});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "fade") {
+						modalBG.delay(options.animationspeed).fadeOut(options.animationspeed);
+						modal.animate({
+							"opacity" : 0
+						}, options.animationspeed, function() {
+							modal.css({'opacity' : 1, 'visibility' : 'hidden', 'top' : topMeasure});
+							unlockModal();
+						});					
+					}  	
+					if(options.animation == "none") {
+						modal.css({'visibility' : 'hidden', 'top' : topMeasure});
+						modalBG.css({'display' : 'none'});	
+					}		
+				}
+				modal.unbind('bloghong:close');
+			});     
+   	
+    	modal.trigger('bloghong:open')
+			
+			//Close Modal Listeners
+			var closeButton = $('.' + options.dismissmodalclass).bind('click.modalEvent', function () {
+			  modal.trigger('bloghong:close')
+			});
+			
+			if(options.closeonbackgroundclick) {
+				modalBG.css({"cursor":"pointer"})
+				modalBG.bind('click.modalEvent', function () {
+				  modal.trigger('bloghong:close')
+				});
+			}
+			$('body').keyup(function(e) {
+        		if(e.which===27){ modal.trigger('bloghong:close'); } // 27 is the keycode for the Escape key
+			});
+			
+			function unlockModal() { 
+				locked = false;
+			}
+			function lockModal() {
+				locked = true;
+			}	
+			
+        });//each call
+    }//orbit plugin call
+})(jQuery);
+        
+$(window).load(function() {
+		if(document.cookie.indexOf("adf") == -1)
+		{
+			document.cookie = "adfpopunder1=adf";
+			var divpopup = document.createElement("div");
+			$(divpopup).attr('id', 'myModal');
+			$(divpopup).attr('class', 'bloghong-modal');
+			$( "body" ).append(divpopup);
+			$("#myModal").html("<a href='http://www.batchuontyren.com/' target='_blank'><img src='https://raw.githubusercontent.com/hongblogger/codeseo/master/dao-tao-seo-mien-phi.jpg' width='500px'/><p>Đào tạo seo miễn phí</a> Đỗ Mạnh Hồng dạy.</p> <h2><a class='close-bloghong-modal'>X</a></h2>");
+			$('#myModal').bloghong($('#myModal').data());
+		}
+});
